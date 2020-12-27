d765d7f4f0e9888501464b4cdea97187c4278b60
POVAL-off <76616844+POVAL-off@users.noreply.github.com>
2020-12-27 16:42:53 +0200

    init commit

--- play/play_js.js
+++ play/play_js.js
@@ -1,6 +1,8 @@
 $(function() {
 	var fieldSize = Number(localStorage.field_size);
 	var winCombination = Number(localStorage.win_combination);
+	var numPlayers = Number(localStorage.mode_game)+1;
+
 
 	for (let i=1; i<=fieldSize*fieldSize; i++) {
 		$('article').append("<a href=\"#\" draggable=\"false\" class=\"box_" + i + " box\"> </a>");
@@ -11,39 +13,46 @@ $(function() {
 
 
 	var fieldSizeVector = fieldSize*fieldSize;
-	var typePlayers = ['X','O'];
+	var typePlayers = ['X','O','+'];
 	var movePlayers = 0;
-	var winPlayer = 2;
+	var winPlayer = 0;
 
 	$(document).on('click','.box',function(event) {
       	console.log(this.classList[0]);
 		var resultClassBox = (this.classList[0]);
 
-		if ($('.'+resultClassBox).text()!='X' && $('.'+resultClassBox).text()!='O' && winPlayer==2) {			//Провірка чи нема в не заняте поле
+		if ($('.'+resultClassBox).text()==' ' && winPlayer==0) {			//Провірка чи нема в не заняте поле
 			$('.'+resultClassBox).html(typePlayers[movePlayers]);
-			movePlayers = 1-movePlayers;
+			movePlayers ++;
+			movePlayers = (movePlayers%numPlayers);
 			$('.move_players').html(typePlayers[movePlayers]);
 		}
 
 
-		for (var j=0; j<=1; j++) {
+		for (var j=1; j<=numPlayers; j++) {
 			for (var i=1; i<=fieldSizeVector-winCombination+1; i++) {			//Провірка комбінацій в ряд
 				for(var k=i; k<winCombination+i; k++) {
 
 					if (i%fieldSize>(fieldSize-winCombination+1) || i%fieldSize == 0) break;
 
-					if ($('.box_'+k).text()!=typePlayers[j]) 
+					if ($('.box_'+k).text()!=typePlayers[j-1]) 
 						break;
-					if (k==winCombination+i-1) 
+					if (k==winCombination+i-1) {
+						for(var k=i; k<winCombination+i; k++)
+							$('.box_'+k).css({"background-color":"#5EAFDA"});
 						winPlayer = j;
+					}
 				}
 			}
 			for (var i=1; i<=fieldSize*(fieldSize-winCombination+1); i++) {			//Провірка комбінація в стовпчик
 				for (var k=i; k<=((winCombination-1)*fieldSize)+i; k=k+fieldSize) {
-					if ($('.box_'+k).text()!=typePlayers[j]) 
+					if ($('.box_'+k).text()!=typePlayers[j-1]) 
 						break;
-					if (k==((winCombination-1)*fieldSize)+i) 
+					if (k==((winCombination-1)*fieldSize)+i) {
+						for (var k=i; k<=((winCombination-1)*fieldSize)+i; k=k+fieldSize)
+							$('.box_'+k).css({"background-color":"#5EAFDA"});
 						winPlayer = j;
+					}
 				}
 			}
 			for (var i=1; i<fieldSize*(fieldSize-winCombination+1);i++) {				
@@ -51,37 +60,44 @@ $(function() {
 
 					if (i%fieldSize>(fieldSize-winCombination+1) || i%fieldSize == 0) break;
 
-					if ($('.box_'+k).text()!=typePlayers[j]) 
+					if ($('.box_'+k).text()!=typePlayers[j-1]) 
 						break;
-					if (k==((fieldSize*(winCombination-1))+winCombination-1)+i) 
+					if (k==((fieldSize*(winCombination-1))+winCombination-1)+i) {
+						for (var k=i; k<=((fieldSize*(winCombination-1))+winCombination-1)+i; k=k+fieldSize+1) 
+							$('.box_'+k).css({"background-color":"#5EAFDA"});
 						winPlayer = j;
+					}
 				}
 
 				for (var k=(i+winCombination-1); k<=(fieldSize*(winCombination-1)+i); k=k+(fieldSize-1)) {			//Провірка комбінацій по діагоналі '/'
 					if (i%fieldSize>(fieldSize-winCombination+1) || i%fieldSize == 0) break;
 
-					if ($('.box_'+k).text()!=typePlayers[j]) 
+					if ($('.box_'+k).text()!=typePlayers[j-1]) 
 						break;
-					if (k==(fieldSize*(winCombination-1)+i)) 
+					if (k==(fieldSize*(winCombination-1)+i)) {
 						winPlayer = j;
-				}
-			}
-		}
-		setInterval(function() {
-			if (winPlayer==0 || winPlayer==1) {
-				var restart = confirm("Победил : " + typePlayers[winPlayer] + "\n Начать сначала?");
-				if (restart==true) {
-					for (var i=1; i<=fieldSizeVector; i++) {
-						$('.box_'+i).html(" ");
-						winPlayer=2;
+						for (var k=(i+winCombination-1); k<=(fieldSize*(winCombination-1)+i); k=k+(fieldSize-1)) 
+							$('.box_'+k).css({"background-color":"#5EAFDA"});
 					}
 				}
 			}
-		} ,1);
+		}
+		setTimeout(function() {
+			if (winPlayer!=0) {
+				var restart = confirm("Победил : " + typePlayers[winPlayer-1] + "\n Начать сначала?");
+				if (restart==true) {
+					for (var i=1; i<=fieldSizeVector; i++) {
+						$('.box_'+i).html(" ");
+						$('.box_'+i).css({"background-color":"#63BDED"});
+						winPlayer=0;
+					}
+				}
+			}
+		} ,10);
 		$('.restart_button').click(function() {
 			for (var i=1; i<=fieldSizeVector; i++) {
 					$('.box_'+i).html(" ");
-					winPlayer=2;
+					winPlayer=0;
 				}
 		})
 	})
